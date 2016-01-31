2439820f0ccfc52818f2c156c106b4f61313a60e
Nicolas Fetter <nicofetter@gmail.com>
2016-02-01 10:50:12 +1300

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name harryAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the harryAppApp
+ */
+angular.module('harryAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name harryAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the harryAppApp
+ */
+angular.module('harryAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
