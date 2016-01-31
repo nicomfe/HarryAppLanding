3f9ae523c35d506c6d690f4509b06cbff0be7ea7
Nicolas Fetter <nicofetter@gmail.com>
2016-02-01 12:06:36 +1300

    Initial commit for fullstack app

---
+++ client/app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('harryAppLandingApp', [
+  'harryAppLandingApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
